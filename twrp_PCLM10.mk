3606a46baaa8bee53bf6184f6d1f61287f40a2e2
donsenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-09-24 00:39:53 +0800

    PCLM10: Nuke not exist in A11

--- twrp_PCLM10.mk
+++ twrp_PCLM10.mk
@@ -17,7 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, build/target/product/embedded.mk)
+#$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
